447f14f6c97518892e6fa6a2b1a245da6e13f528
Alex Mateo <va_mateo@outlook.com>
2022-11-18 16:52:27 -0500

    Adding Add/Remove funtionality to the Cart feature

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { ThisReceiver } from "@angular/compiler";
 import { Component, OnInit } from "@angular/core";
+import { TitleStrategy } from "@angular/router";
 import { Cart, CartItem } from "src/app/models/cart";
 import { CartService } from "src/app/services/cart.service";
 
@@ -42,9 +43,29 @@ export class CartComponent implements OnInit {
 
   ngOnInit(): void {
     this.dataSource = this.cart.items;
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    })
   }
 
   getTotal(items: Array<CartItem>): number {
     return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveCart(item: CartItem ): void {
+    this.cartService.removeFromCart(item);
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
   }
 }
